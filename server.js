a4c051570752b3075645e69b766cbd6013d79d55
Nounours Heureux <schtroumps31@gmail.com>
2015-02-20 19:36:09 +0100

    Removed the update function

--- server.js
+++ server.js
@@ -13,44 +13,35 @@ app.use(express.static(path.join(__dirname,'public')));
 //Global declarations for easy configuration
 
 global.song = function(){
-	exec('mpc current',function(err,stdout,stderr) {
+    exec('mpc current',function(err,stdout,stderr) {
         if(err) throw err;
         if(stdout) {
             io.emit('song',stdout.toString('utf8'));
         }
     });
-}
+};
 
 global.disk = function(){
-	exec("echo \"$(df -h "+ config.partition + " | sed '1d' | awk '{print $3}')/$(df -h " + config.partition + " | sed '1d' | awk '{print $2}')\"",function(err,stdout,stderr){
+    exec("echo \"$(df -h "+ config.partition + " | sed '1d' | awk '{print $3}')/$(df -h " + config.partition + " | sed '1d' | awk '{print $2}')\"",function(err,stdout,stderr){
         if(err) throw err;
         if(stdout) {
             io.emit('disk',stdout.toString());
         }
     });
-}
+};
 
 global.ram = function(){
-	exec("echo \"$(free -h | head -n 2 | tail -n 1 | awk '{print $4}')/$(free -h | head -n 2 | tail -n 1 | awk '{print $2}')\"",function(err,stdout,stderr){
+    exec("echo \"$(free -h | head -n 2 | tail -n 1 | awk '{print $4}')/$(free -h | head -n 2 | tail -n 1 | awk '{print $2}')\"",function(err,stdout,stderr){
         if(err) throw err;
         if(stdout) {
             io.emit('ram',stdout.toString('utf8'));
         }
     });
 
-}
-
-global.update = function(){
-	exec("checkupdates | wc -l",function(err,stdout,stderr){
-        if(err) throw err;
-        if(stdout) {
-            io.emit('update',stdout.toString('utf8'));
-        }
-    });
-}
+};
 
 global.weather = function(){
-	http.get("http://api.openweathermap.org/data/2.5/weather?q="+config.city,function(res){
+    http.get("http://api.openweathermap.org/data/2.5/weather?q="+config.city,function(res){
         var str = '';
         res.on('data',function(chunk){
             str += chunk;
@@ -59,16 +50,16 @@ global.weather = function(){
             io.emit('weather',str);
         });
     });
-}
+};
 
 //Initialize the modules
 
 io.on('connection',function(socket){
-	config.modules.forEach(function(val){
-		if(typeof global[val.name] === "function"){
-			global[val.name]();
-		}
-	})
+    config.modules.forEach(function(val){
+        if(typeof global[val.name] === "function"){
+            global[val.name]();
+        }
+    });
     socket.emit('config',config);
     socket.on('command',function(command){
         exec(command);
@@ -76,7 +67,7 @@ io.on('connection',function(socket){
 });
 
 config.modules.forEach(function(val){
-	if(typeof global[val.name] === "function"){
-		setInterval(global[val.name],val.interval);
-	}
+    if(typeof global[val.name] === "function"){
+        setInterval(global[val.name],val.interval);
+    }
 });
