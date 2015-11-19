cf4233e98db708915b123559152e7085ab6551a5
Nounours Heureux <schtroumps31@gmail.com>
2015-11-19 16:37:24 +0100

    Don't crash when an error occurs - Use node functions instead of the uname command

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     http = require('http'),
     path = require('path'),
+    os = require('os'),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     exec = require('child_process').exec,
@@ -12,12 +13,20 @@ app.use(express.static(path.join(__dirname,'public')));
 
 console.log('Listening on port ' + config.port);
 
+var intervals = {};
+
+function handleError(moduleName, error) {
+  console.error('Error in module ' + moduleName + ': ' + error);
+  clearInterval(intervals[moduleName]);
+}
+
 //Global declarations for easy configuration
 
 global.song = function(){
     exec('mpc current',function(err,stdout,stderr) {
-        if(err) throw err;
-        if(stdout) {
+        if(err) {
+          handleError('song', err);
+        } else if(stdout) {
             io.emit('song',stdout.toString('utf8'));
         }
     });
@@ -25,8 +34,9 @@ global.song = function(){
 
 global.disk = function(){
     exec("echo \"$(df "+ config.partition +" --output=used -h |sed '1d')/$(df "+ config.partition +" --output=size -h |sed '1d')\" available",function(err,stdout,stderr){
-        if(err) throw err;
-        if(stdout) {
+        if(err) {
+            handleError('disk', err);
+        } else if(stdout) {
             io.emit('disk',stdout.toString());
         }
     });
@@ -34,12 +44,12 @@ global.disk = function(){
 
 global.ram = function(){
     exec("echo \"$(free -h | awk '/Mem:/ {print $3}')/$(free -h | awk '/Mem:/ {print $2}') used\"",function(err,stdout,stderr){
-        if(err) throw err;
-        if(stdout) {
+        if(err) {
+            handleError('ram', err);
+        } else if(stdout) {
             io.emit('ram',stdout.toString('utf8'));
         }
     });
-
 };
 
 global.weather = function(){
@@ -55,12 +65,7 @@ global.weather = function(){
 };
 
 global.uname = function(){
-    exec('uname -r',function(err,stdout,stderr) {
-        if(err) throw err;
-        if(stdout) {
-            io.emit('uname',stdout.toString('utf8'));
-        }
-    });
+    io.emit('uname', os.type() + ' ' + os.release() + ' ' + os.arch());
 };
 //Initialize the modules
 
@@ -78,6 +83,6 @@ io.on('connection',function(socket){
 
 config.modules.forEach(function(val){
     if(typeof global[val.name] === "function"){
-        setInterval(global[val.name],val.interval);
+        intervals[val.name] = setInterval(global[val.name],val.interval);
     }
 });
